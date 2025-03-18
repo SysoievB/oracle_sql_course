6abf7ed8ebd36d09725caec81366b9835f83186a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-03-18 18:05:54 +0100

    Created project

---
+++ src/main/java/com/oracle_sql_course/OracleSqlCourseApplication.java
@@ -0,0 +1,13 @@
+package com.oracle_sql_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleSqlCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleSqlCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oracle_sql_course/OracleSqlCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle_sql_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleSqlCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
