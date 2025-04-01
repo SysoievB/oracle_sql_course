4c4710ce140c74eac8c27cb69b47c330471b0b38
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-04-01 07:38:49 +0200

    Added lesson 8

--- src/main/resources/sql_tasks/subquery.sql
+++ src/main/resources/sql_tasks/subquery.sql
@@ -13,13 +13,35 @@ where LENGTH(FIRST_NAME) = (SELECT MAX(LENGTH(FIRST_NAME)) FROM EMPLOYEES);-- re
 
 -- Выведите всю информацию  о сотрудниках, с зарплатой большей
 -- средней зарплаты всех сотрудников.
+select FIRST_NAME,
+       SALARY
+from EMPLOYEES
+where SALARY > (select avg(SALARY) from EMPLOYEES);
 
 -- Получить город/города, где сотрудники в сумме зарабатывают меньше всего.
-
--- Выведите всю информацию о сотрудниках, у которых менеджер
--- получает зарплату больше 15000.
+SELECT l.CITY, SUM(e.SALARY) AS TOTAL_SALARY
+FROM LOCATIONS l
+         JOIN DEPARTMENTS d ON l.LOCATION_ID = d.LOCATION_ID
+         JOIN EMPLOYEES e ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+GROUP BY l.CITY
+HAVING SUM(e.SALARY) = (SELECT MIN(TOTAL_SALARY)
+                        FROM (SELECT SUM(e.SALARY) AS TOTAL_SALARY
+                              FROM EMPLOYEES e
+                                       JOIN DEPARTMENTS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+                                       JOIN LOCATIONS l ON d.LOCATION_ID = l.LOCATION_ID
+                              GROUP BY l.CITY));
 
 -- Выведите всю информацию о департаментах, в которых нет ни одного сотрудника.
+SELECT
+    d.DEPARTMENT_ID,
+    d.DEPARTMENT_NAME
+FROM DEPARTMENTS d
+WHERE
+    d.DEPARTMENT_ID NOT IN (
+        SELECT DISTINCT DEPARTMENT_ID
+        FROM EMPLOYEES
+        WHERE DEPARTMENT_ID IS NOT NULL
+    );
 
 -- Выведите всю информацию  о сотрудниках, которые не являются менеджерами.
 
